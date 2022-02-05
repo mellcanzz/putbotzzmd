64c8a16ed5bca2101b8d247c44b1fc2000a1afb4
putbotzz <putrawan461@gmail.com>
2022-02-05 23:15:36 +0800

    putbotzzz

---
+++ ryudata/sewa.js
@@ -0,0 +1,23 @@
+exports.sewa = () => {
+	return`🇸 🇪 🇼 🇦  🇧 🇴 🇹 ❗
+
+┏━━━━━━
+┃𝗛𝗔𝗥𝗚𝗔 𝗦𝗘𝗪𝗔 :
+┃ *1 MINGGU : Rm 5*
+┃ *1 BULAN : Rm 15*
+┃ *PERMANEN : Rm 20*
+┗━━━━━━
+┏━━━━━━
+┃𝗙𝗜𝗧𝗨𝗥 𝗕𝗢𝗧 :
+┃ *⚔️ Antilink Group*
+┃ *👨‍🎨 Buat Stiker*
+┃ *📥 Downloader*
+┃ *Dll.*
+┗━━━━━━
+
+*SISTEM: TES BOT-MASUKIN BOT-TF-DONE*
+
+*MINAT? CHAT :
+wa.me/60147366955
+`
+}
---
+++ ryudata/welkam.js
@@ -0,0 +1,22 @@
+//EDIT SESUKA KALIAN 😉
+
+// UCAPAN WELCOME
+exports.welkam = () => {
+	return `Intro Dulu Yuk Biar Keren 🤙
+
+📛 Nama : 
+🔞 Umur :
+
+Semoga Kamu Senang Berada Disini
+Serta Jangan Lupa Untuk Membaca
+Dan Mematuhi Rules Yang Ada
+`
+}
+
+// UCAPAN GOODBYE
+exports.goodbye = () => {
+	return `Tetap Putus Asa Jangan
+Semangat Dan Jadilah Beban
+Keluarga 🤙🗿
+`
+}
